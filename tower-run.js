aa537df56db23d75746df5159d35f2f58955160d
Erik Nordstrøm <erik@nordstroem.no>
2018-02-17 04:55:56 +0100

    Render full ring on its own.

--- tower-run.js
+++ tower-run.js
@@ -16,7 +16,8 @@
 
 const html = document.querySelector('html');
 
-const canv = document.getElementById('game');
+//const canv = document.getElementById('game');
+const canv = document.createElement('canvas');
 const ctx = canv.getContext('2d');
 
 canv.width = window.innerWidth;
@@ -25,16 +26,19 @@ canv.height = window.innerHeight;
 // TODO: Change canvas size when window is resized.
 
 // The width of a brick when viewed from the front is specified in radians.
-const brickwidth_rad = 2 * Math.PI / 256;
+const num_bricks_around = 256;
+const brickwidth_rad = 2 * Math.PI / num_bricks_around;
 
 let angular_velocity = 20 * brickwidth_rad; // Unit: radians / second
 let angular_acceleration = 0; // Unit: rads / s^2
+
+// Bricks
 
 brick = document.createElement('canvas');
 bctx = brick.getContext('2d');
 
 brickratio = 0.5;
-brick.width = 512;
+brick.width = 64;
 brick.height = Math.floor(brickratio * brick.width);
 
 bctx.fillStyle = '#999';
@@ -42,6 +46,70 @@ bctx.fillRect(0, 0, brick.width, brick.height);
 bctx.strokeStyle = '#333';
 bctx.lineWidth = Math.floor(brick.width / 10);
 bctx.strokeRect(0.5, 0.5, brick.width, brick.height);
+
+// Rings of bricks
+
+//const ring = document.createElement('canvas');
+const ring = document.getElementById('ring');
+const rctx = ring.getContext('2d');
+
+ring.width = num_bricks_around * brick.width;
+ring.height = 2 * brick.height;
+
+rctx.drawImage(brick,
+	Math.ceil(0.5 * brick.width), 0,
+	brick.width, brick.height,
+	0, 0,
+	brick.width, brick.height);
+
+for (let i = 0 ; i < num_bricks_around ; i++)
+{
+	ctx.fillStyle = '#333';
+	ctx.fillRect(0, 0, ring.width, ring.height);
+
+	/*
+	 * Top row.
+	 */
+
+	rctx.drawImage(brick,
+		0, 0,
+		brick.width, brick.height,
+		Math.floor(brick.width * (i + 0.5)), 0,
+		brick.width, brick.height);
+
+	/*
+	 * Bottom row.
+	 */
+
+	rctx.drawImage(brick,
+		0, 0,
+		brick.width, brick.height,
+		brick.width * i, brick.height,
+		brick.width, brick.height);
+}
+
+/*
+ * Highlight and shadow on the brick ring.
+ */
+
+const middle_x = Math.floor(0.5 * ring.width);
+const middle_y = Math.floor(0.5 * ring.height);
+
+const grad_high = ctx.createLinearGradient(0, middle_y, middle_x, middle_y);
+grad_high.addColorStop(0, 'transparent');
+grad_high.addColorStop(0.5, 'rgba(255, 255, 255, 0.3)');
+grad_high.addColorStop(1, 'transparent');
+rctx.fillStyle = grad_high;
+rctx.fillRect(0, 0, middle_x, ring.height);
+
+const grad_shad = ctx.createLinearGradient(middle_x, middle_y, ring.width, middle_y);
+grad_shad.addColorStop(0, 'transparent');
+grad_shad.addColorStop(0.5, 'rgba(0, 0, 0, 0.4)');
+grad_shad.addColorStop(1, 'transparent');
+rctx.fillStyle = grad_shad;
+rctx.fillRect(middle_x, 0, middle_x, ring.height);
+
+// Game
 
 let angle = 0;
 
@@ -51,104 +119,14 @@ let t_prev = t_start;
 let dt = 0;
 let dt_recent = new Array(480);
 
-let towerwidth_px;
-let towerradius_px;
-let towerstart_x_px;
-let towerend_x_px;
-
-function render_gradient_if_visible (middle_pos_at_angle, color)
-{
-	let angle_offset_pos_dir = angle - middle_pos_at_angle;
-	let angle_offset_neg_dir = middle_pos_at_angle - angle;
-
-	angle_offset_pos_dir += angle_offset_pos_dir < 0 ? 2 * Math.PI : 0;
-	angle_offset_neg_dir += angle_offset_neg_dir < 0 ? 2 * Math.PI : 0;
-
-	let angle_offset;
-
-	if (angle_offset_pos_dir <= angle_offset_neg_dir)
-	{
-		on_left_side = true;
-		angle_offset = angle_offset_pos_dir;
-	}
-	else
-	{
-		on_left_side = false;
-		angle_offset = angle_offset_neg_dir;
-	}
-
-	let sign;
-
-	if (on_left_side)
-	{
-		sign = -1;
-	}
-	else
-	{
-		sign = 1;
-	}
-
-	if (angle_offset <= 0.5 * Math.PI)
-	{
-		const grad_middle_x_px = towerstart_x_px + towerradius_px + Math.sin(sign * angle_offset) * towerradius_px;
-		const grad_right_width_px = towerradius_px;
-		const grad_left_width_px  = towerradius_px;
-
-		const grad_left = ctx.createLinearGradient(grad_middle_x_px - grad_left_width_px, Math.floor(0.5 * canv.height),
-			grad_middle_x_px, Math.floor(0.5 * canv.height));
-		grad_left.addColorStop(0, 'transparent');
-		grad_left.addColorStop(1, color);
-
-		ctx.fillStyle = grad_left;
-		ctx.fillRect(towerstart_x_px, 0, grad_middle_x_px - towerstart_x_px, canv.height);
-
-		const grad_right = ctx.createLinearGradient(grad_middle_x_px, Math.floor(0.5 * canv.height),
-			grad_middle_x_px + grad_right_width_px, Math.floor(0.5 * canv.height));
-		grad_right.addColorStop(0, color);
-		grad_right.addColorStop(1, 'transparent');
-
-		ctx.fillStyle = grad_right;
-		ctx.fillRect(grad_middle_x_px, 0, towerend_x_px - grad_middle_x_px, canv.height);
-	}
-
-	else if (angle_offset <= Math.PI && on_left_side)
-	{
-		const grad_middle_x_px = towerstart_x_px;
-		const grad_right_width_px = Math.sin(angle_offset) * towerradius_px;
-
-		const grad_right = ctx.createLinearGradient(grad_middle_x_px, Math.floor(0.5 * canv.height),
-			grad_middle_x_px + grad_right_width_px, Math.floor(0.5 * canv.height));
-		grad_right.addColorStop(0, color);
-		grad_right.addColorStop(1, 'transparent');
-
-		ctx.fillStyle = grad_right;
-		ctx.fillRect(grad_middle_x_px, 0, grad_right_width_px, canv.height);
-	}
-
-	else if (angle_offset <= Math.PI && !on_left_side)
-	{
-		const grad_middle_x_px = towerstart_x_px + towerwidth_px;
-		const grad_left_width_px = Math.sin(angle_offset) * towerradius_px;
-
-		const grad_left = ctx.createLinearGradient(
-			grad_middle_x_px - grad_left_width_px, Math.floor(0.5 * canv.height),
-			grad_middle_x_px, Math.floor(0.5 * canv.height));
-		grad_left.addColorStop(0, 'transparent');
-		grad_left.addColorStop(1, color);
-
-		ctx.fillStyle = grad_left;
-		ctx.fillRect(grad_middle_x_px - grad_left_width_px, 0, grad_left_width_px, canv.height);
-	}
-}
-
 function render ()
 {
 	ctx.clearRect(0, 0, canv.width, canv.height);
 
-	towerwidth_px = Math.floor(0.8 * canv.width);
-	towerradius_px = Math.floor(0.5 * towerwidth_px);
-	towerstart_x_px = Math.floor((canv.width - towerwidth_px) / 2);
-	towerend_x_px = towerstart_x_px + towerwidth_px;
+	const towerwidth_px = Math.floor(0.8 * canv.width);
+	const towerradius_px = Math.floor(0.5 * towerwidth_px);
+	const towerstart_x_px = Math.floor((canv.width - towerwidth_px) / 2);
+	const towerend_x_px = towerstart_x_px + towerwidth_px;
 
 	const brickwidth_px = Math.floor(2 * Math.PI * towerradius_px * brickwidth_rad);
 	const brickheight_px = Math.floor(brickratio * brickwidth_px);
@@ -158,91 +136,6 @@ function render ()
 
 	const brickoffs_x_pct = (angle % brickwidth_rad) / brickwidth_rad;
 	const brickoffs_x_srcpx = Math.floor(brickoffs_x_pct * brick.width);
-
-	ctx.fillStyle = '#333';
-	ctx.fillRect(towerstart_x_px, 0, towerwidth_px, canv.height);
-
-	// The two rows middle rows of bricks on screen have no vertical distortion.
-	let curr_x = 0;
-	for (let i = 0 ; i < 100 ; i++)
-	{
-		brickwidth_foreshortened_px = Math.floor(Math.cos((curr_x / towerradius_px) * 0.5 * Math.PI) * brickwidth_px);
-
-		/*
-		 * Top row.
-		 */
-
-		// Place brick on left half of screen.
-		ctx.drawImage(brick,
-			- 1.5 * brick.width + brickoffs_x_srcpx, 0,
-			brick.width, brick.height,
-			middle_x_px - (brickwidth_foreshortened_px + curr_x), middle_y_px - brickheight_px,
-			brickwidth_foreshortened_px, brickheight_px);
-		ctx.drawImage(brick,
-			Math.floor(brickoffs_x_srcpx - 0.5 * brick.width), 0,
-			brick.width, brick.height,
-			middle_x_px - (brickwidth_foreshortened_px + curr_x), middle_y_px - brickheight_px,
-			brickwidth_foreshortened_px, brickheight_px);
-		ctx.drawImage(brick,
-			Math.floor(0.5 * brick.width + brickoffs_x_srcpx), 0,
-			brick.width, brick.height,
-			middle_x_px - (brickwidth_foreshortened_px + curr_x), middle_y_px - brickheight_px,
-			brickwidth_foreshortened_px, brickheight_px);
-
-		// Place brick on right half of screen.
-		ctx.drawImage(brick,
-			- 1.5 * brick.width + brickoffs_x_srcpx, 0,
-			brick.width, brick.height,
-			middle_x_px + curr_x, middle_y_px - brickheight_px,
-			brickwidth_foreshortened_px, brickheight_px);
-		ctx.drawImage(brick,
-			Math.floor(brickoffs_x_srcpx - 0.5 * brick.width), 0,
-			brick.width, brick.height,
-			middle_x_px + curr_x, middle_y_px - brickheight_px,
-			brickwidth_foreshortened_px, brickheight_px);
-		ctx.drawImage(brick,
-			Math.floor(0.5 * brick.width + brickoffs_x_srcpx), 0,
-			brick.width, brick.height,
-			middle_x_px + curr_x, middle_y_px - brickheight_px,
-			brickwidth_foreshortened_px, brickheight_px);
-
-		/*
-		 * Bottom row.
-		 */
-
-		// Place brick on left half of screen.
-		ctx.drawImage(brick,
-			brickoffs_x_srcpx, 0,
-			brick.width, brick.height,
-			middle_x_px - (brickwidth_foreshortened_px + curr_x), middle_y_px,
-			brickwidth_foreshortened_px, brickheight_px);
-		ctx.drawImage(brick,
-			-brick.width + brickoffs_x_srcpx, 0,
-			brick.width, brick.height,
-			middle_x_px - (brickwidth_foreshortened_px + curr_x), middle_y_px,
-			brickwidth_foreshortened_px, brickheight_px);
-
-		// Place brick on right half of screen.
-		ctx.drawImage(brick,
-			brickoffs_x_srcpx, 0,
-			brick.width, brick.height,
-			middle_x_px + curr_x, middle_y_px,
-			brickwidth_foreshortened_px, brickheight_px);
-		ctx.drawImage(brick,
-			-brick.width + brickoffs_x_srcpx, 0,
-			brick.width, brick.height,
-			middle_x_px + curr_x, middle_y_px,
-			brickwidth_foreshortened_px, brickheight_px);
-
-		curr_x += brickwidth_foreshortened_px;
-	}
-
-	/*
-	 * Highlight and shadow on tower itself.
-	 */
-
-	render_gradient_if_visible(0, 'rgba(255, 255, 255, 0.3)');
-	render_gradient_if_visible(Math.PI, 'rgba(0, 0, 0, 0.7)');
 
 	// End highlight and shadow.
 
@@ -281,4 +174,4 @@ function run ()
 
 // TODO: Settings screen where keybindings can be configured.
 
-window.requestAnimationFrame(run);
+//window.requestAnimationFrame(run);
